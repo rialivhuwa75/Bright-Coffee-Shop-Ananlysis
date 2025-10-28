fb29ee71a59afa32568c1882471636f1f06bfcdf
Rialivhuwa Nevhungoni <rialivhuwa75@gmail.com>
2025-10-28 16:37:16 +0200

    Add SQL queries for coffee sales analysis

---
+++ Bright.coffee.shop.sql
@@ -0,0 +1,66 @@
+-------------------------------------------------------------------------------------------
+--To check the data types in my data
+SELECT *
+FROM projectcoffee.bright.coffee_sales
+LIMIT 10;
+
+-------------------------------------------------------------------------------------------
+--I want to check my categorical columns
+SELECT DISTINCT store_location
+FROM projectcoffee.bright.coffee_sales;
+
+-------------------------------------------------------------------------------------------
+--To check types of products
+SELECT DISTINCT product_category
+FROM projectcoffee.bright.coffee_sales;
+
+-------------------------------------------------------------------------------------------
+--To check when was the shop opened
+SELECT MIN(transaction_date) AS first_operating_date
+FROM projectcoffee.bright.coffee_sales;
+
+-------------------------------------------------------------------------------------------
+--To check the last oprating date
+SELECT MAX(transaction_date) AS last_operating_date
+FROM projectcoffee.bright.coffee_sales;
+-------------------------------------------------------------------------------------------
+--To check at what time does the shop open
+SELECT MIN(transaction_time) AS opening_hour
+FROM projectcoffee.bright.coffee_sales;
+-------------------------------------------------------------------------------------------
+--To check at what time does the shop close
+SELECT MAX(transaction_time) AS closing_hour
+FROM projectcoffee.bright.coffee_sales;
+
+------------------------------------------------------------------------------------------
+SELECT transaction_date,
+       DAYNAME(transaction_date) AS day_name,
+       CASE
+        WHEN day_name IN ('Sun', 'Sat') THEN 'Weekend'
+        ELSE 'Weekday'
+       END AS day_classification,
+       MONTHNAME(transaction_date) AS month_name,
+       
+       transaction_time,
+       CASE
+        WHEN transaction_time BETWEEN '06:00:00' AND '11:59:59' THEN 'Morning'
+        WHEN transaction_time BETWEEN '12:00:00' AND '16:59:59' THEN 'Afternoon'
+        WHEN transaction_time >= '17:00:00' THEN 'Night'
+       END AS time_bucket,
+       
+       HOUR (transaction_time) AS hour_of_day,
+       --categorical data
+       store_location,
+       product_category,
+       product_detail,
+       product_type,
+
+       --ID'S
+       COUNT(DISTINCT transaction_id) AS number_of_sales,
+
+       --Revenue calculation
+       SUM(transaction_qty*unit_price) AS revenue
+       
+FROM projectcoffee.bright.coffee_sales
+GROUP BY ALL
+ORDER BY revenue DESC;
